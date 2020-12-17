b09a0166606ab456de23dc1f37d78857ed1f9c4d
Jacob Linacre <jacob.linacre@gmail.com>
2020-12-17 19:05:06 +0000

    hack for selecting nGluino=0 component for 2018 'squark' samples

--- PATNTupler/src_macros/PlotEntry2D.cc
+++ PATNTupler/src_macros/PlotEntry2D.cc
@@ -155,6 +155,75 @@ void PlotEntry2D::AddInput(const std::string& flatTreeAddress, const std::string
 }
 
 
+void PlotEntry2D::AddInput2018squark(const std::string& flatTreeAddress, const std::string& selectionCut, const double& crossSection, const std::string& scaleFactorWeightStr)
+{
+	TFile * f = TFile::Open(flatTreeAddress.c_str());
+    TTree * evT = (TTree*)f->Get("eventCountTree");
+    Int_t evTEntries = (Int_t)evT->GetEntries();
+	UInt_t nEvtsRunOverForInputEntry;
+	UInt_t nEvtsAfterPreSelForInputEntry;
+	evT->SetBranchAddress("nEvtsRunOver",&nEvtsRunOverForInputEntry);
+	evT->SetBranchAddress("nEvtsPass",&nEvtsAfterPreSelForInputEntry);        
+    UInt_t nEvtsRunOverForInputTotal = 0;
+    unsigned int numberOfEventsAfterPreSelection = 0;
+    for (Int_t ievT=0; ievT<evTEntries; ++ievT){
+        evT->GetEntry(ievT);
+        nEvtsRunOverForInputTotal += nEvtsRunOverForInputEntry;
+        numberOfEventsAfterPreSelection += nEvtsAfterPreSelForInputEntry;
+    }
+    numberOfEventsBeforeCuts += 1000.0 * crossSection * luminosity;
+
+	TTree * T = (TTree*)f->Get("doubleBFatJetPairTree");
+
+	double ncor = T->GetEntries("nGluino==0")*nEvtsRunOverForInputTotal/double(numberOfEventsAfterPreSelection);
+
+	if (T->GetEntries("nGluino==0") != numberOfEventsAfterPreSelection){
+		std::cout << "number of events " << T->GetEntries("nGluino==0") << " in doubleBFatJetPairTree of " << flatTreeAddress << " does not equal those in the nEvtsPass count " << numberOfEventsAfterPreSelection << std::endl;
+		std::cout << "number of events in doubleBFatJetPairTree " << T->GetEntries("nGluino==0") << " correction factor: " << nEvtsRunOverForInputTotal/numberOfEventsAfterPreSelection << " corrected: " << ncor << std::endl;
+		// return;
+	}
+
+    double eventWeighting = 1000.0 * crossSection * luminosity / ncor;
+
+	TH2D hContainer = hNull; // make a copy of the empty histogram to fill with TTreeDraw
+	hContainer.SetName("hContainer");
+	std::string drawStringA = Form("%s>>hContainer", variablesToPlot.c_str());
+	std::string drawStringB;
+	if (!selectionCut.empty()) drawStringB = Form("%.15f*(%s)", eventWeighting, selectionCut.c_str());
+	else drawStringB = Form("%.15f", eventWeighting);
+	if (!scaleFactorWeightStr.empty()) drawStringB += Form("*(%s)", scaleFactorWeightStr.c_str());
+    std::cout << "Filling for TTree: " << flatTreeAddress << std::endl;
+	std::cout << "Variables used: " << variablesToPlot << std::endl;
+	if (!scaleFactorWeightStr.empty()) std::cout << ">>> Using Scale Factor Weights" << std::endl;
+	else std::cout << ">>> NOT Using Scale Factor Weights" << std::endl;
+	if (!selectionCut.empty()) std::cout << "Event Weighting * Cut applied: " << drawStringB << std::endl;
+	else std::cout << "Event Weighting: " << drawStringB << "\nNB: no cut applied." << std::endl;
+	T->Draw(drawStringA.c_str(), drawStringB.c_str(), "");
+	
+	double scaleFactorWeight = 1.0; // need to find average scale factor weight
+	if (!scaleFactorWeightStr.empty()){
+		TH1D * hSFW = new TH1D("hSFW", "", 1, -99999.9, 99999.9);
+		std::string drawStringA_sf = Form("(%s)>>hSFW", scaleFactorWeightStr.c_str());
+		T->Draw(drawStringA_sf.c_str(), selectionCut.c_str(), "");
+		if (hSFW->GetMean() != 0) scaleFactorWeight = hSFW->GetMean();
+		// std::cout << "Average Scale Factor Weighting: " << scaleFactorWeight << std::endl;
+	}
+
+	for (int iBin = 0; iBin < hContainer.GetNbinsX()+2; ++iBin){
+		for (int jBin = 0; jBin < hContainer.GetNbinsY()+2; ++jBin){
+			Int_t binLabel = hContainer.GetBin(iBin,jBin);
+			hTotal->AddBinContent(binLabel, hContainer.GetBinContent(binLabel));
+			numberOfEventsAfterCuts_StatErrorSquared += hContainer.GetBinContent(binLabel) * eventWeighting * scaleFactorWeight;
+		}	
+	}
+	delete T;
+	delete evT;
+	delete f;
+	std::cout << std::endl;
+}
+
+
+
 
 void PlotEntry2D::AddInputFromGrid(const std::string& flatTreeAddress, const std::string& selectionCut, const double& crossSection, const std::string& scaleFactorWeightStr)
 {
