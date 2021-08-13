dd067f9a37514aeba88278a47a81b9f4c66318e3
Jacob Linacre <jacob.linacre@gmail.com>
2021-08-13 12:14:25 +0100

    add BRs for new mass points

--- PATNTupler/macros/plotting_brazil.cc
+++ PATNTupler/macros/plotting_brazil.cc
@@ -28,8 +28,11 @@ std::map<std::string, double> xsecBR{
 
 {"mH30_mSusy800",  6.4720000*0.868*0.868},
 {"mH50_mSusy800",  6.4720000*0.858*0.858},
+{"mH60_mSusy800",  6.4720000*0.850*0.850},
 {"mH70_mSusy800",  6.4720000*0.840*0.840},
+{"mH80_mSusy800",  6.4720000*0.829*0.829},
 {"mH90_mSusy800",  6.4720000*0.816*0.816},
+{"mH100_mSusy800",  6.4720000*0.795*0.795},
 {"mH110_mSusy800", 6.4720000*0.749*0.749},
 {"mH125_mSusy800", 6.4720000*0.581*0.581},
 
@@ -37,8 +40,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy1200", 0.4951000*0.867*0.867},
 {"mH40_mSusy1200", 0.4951000*0.865*0.865},
 {"mH50_mSusy1200", 0.4951000*0.858*0.858},
+{"mH60_mSusy1200", 0.4951000*0.850*0.850},
 {"mH70_mSusy1200", 0.4951000*0.840*0.840},
+{"mH80_mSusy1200", 0.4951000*0.829*0.829},
 {"mH90_mSusy1200", 0.4951000*0.816*0.816},
+{"mH100_mSusy1200", 0.4951000*0.795*0.795},
 {"mH110_mSusy1200",0.4951000*0.749*0.749},
 {"mH125_mSusy1200",0.4951000*0.581*0.581},
 
@@ -46,8 +52,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy1600", 0.0603900*0.867*0.867},
 {"mH40_mSusy1600", 0.0603900*0.865*0.865},
 {"mH50_mSusy1600", 0.0603900*0.858*0.858},
+{"mH60_mSusy1600", 0.0603900*0.850*0.850},
 {"mH70_mSusy1600", 0.0603900*0.840*0.840},
+{"mH80_mSusy1600", 0.0603900*0.829*0.829},
 {"mH90_mSusy1600", 0.0603900*0.816*0.816},
+{"mH100_mSusy1600", 0.0603900*0.795*0.795},
 {"mH110_mSusy1600",0.0603900*0.749*0.749},
 {"mH125_mSusy1600",0.0603900*0.581*0.581},
 
@@ -55,8 +64,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy2000", 0.0091050*0.867*0.867},
 {"mH40_mSusy2000", 0.0091050*0.865*0.865},
 {"mH50_mSusy2000", 0.0091050*0.858*0.858},
+{"mH60_mSusy2000", 0.0091050*0.850*0.850},
 {"mH70_mSusy2000", 0.0091050*0.840*0.840},
+{"mH80_mSusy2000", 0.0091050*0.829*0.829},
 {"mH90_mSusy2000", 0.0091050*0.816*0.816},
+{"mH100_mSusy2000", 0.0091050*0.795*0.795},
 {"mH110_mSusy2000",0.0091050*0.749*0.749},
 {"mH125_mSusy2000",0.0091050*0.581*0.581},
 
@@ -64,8 +76,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy2200", 0.0036780*0.867*0.867},
 {"mH40_mSusy2200", 0.0036780*0.865*0.865},
 {"mH50_mSusy2200", 0.0036780*0.858*0.858},
+{"mH60_mSusy2200", 0.0036780*0.850*0.850},
 {"mH70_mSusy2200", 0.0036780*0.840*0.840},
+{"mH80_mSusy2200", 0.0036780*0.829*0.829},
 {"mH90_mSusy2200", 0.0036780*0.816*0.816},
+{"mH100_mSusy2200", 0.0036780*0.795*0.795},
 {"mH110_mSusy2200",0.0036780*0.749*0.749},
 {"mH125_mSusy2200",0.0036780*0.581*0.581},
 
@@ -73,8 +88,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy2400", 0.0015050*0.867*0.867},
 {"mH40_mSusy2400", 0.0015050*0.865*0.865},
 {"mH50_mSusy2400", 0.0015050*0.858*0.858},
+{"mH60_mSusy2400", 0.0015050*0.850*0.850},
 {"mH70_mSusy2400", 0.0015050*0.840*0.840},
+{"mH80_mSusy2400", 0.0015050*0.829*0.829},
 {"mH90_mSusy2400", 0.0015050*0.816*0.816},
+{"mH100_mSusy2400", 0.0015050*0.795*0.795},
 {"mH110_mSusy2400",0.0015050*0.749*0.749},
 {"mH125_mSusy2400",0.0015050*0.581*0.581},
 
@@ -82,8 +100,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy2600", 0.0006167*0.867*0.867},
 {"mH40_mSusy2600", 0.0006167*0.865*0.865},
 {"mH50_mSusy2600", 0.0006167*0.858*0.858},
+{"mH60_mSusy2600", 0.0006167*0.850*0.850},
 {"mH70_mSusy2600", 0.0006167*0.840*0.840},
+{"mH80_mSusy2600", 0.0006167*0.829*0.829},
 {"mH90_mSusy2600", 0.0006167*0.816*0.816},
+{"mH100_mSusy2600", 0.0006167*0.795*0.795},
 {"mH110_mSusy2600",0.0006167*0.749*0.749},
 {"mH125_mSusy2600",0.0006167*0.581*0.581},
 
@@ -91,8 +112,11 @@ std::map<std::string, double> xsecBR{
 {"mH35_mSusy2800", 0.0002753*0.867*0.867},
 {"mH40_mSusy2800", 0.0002753*0.865*0.865},
 {"mH50_mSusy2800", 0.0002753*0.858*0.858},
+{"mH60_mSusy2800", 0.0002753*0.850*0.850},
 {"mH70_mSusy2800", 0.0002753*0.840*0.840},
+{"mH80_mSusy2800", 0.0002753*0.829*0.829},
 {"mH90_mSusy2800", 0.0002753*0.816*0.816},
+{"mH100_mSusy2800", 0.0002753*0.795*0.795},
 {"mH110_mSusy2800",0.0002753*0.749*0.749},
 {"mH125_mSusy2800",0.0002753*0.581*0.581},
 
@@ -175,8 +199,11 @@ std::map<std::string, double> xsecBRNNLL{
 
 {"mH30_mSusy800",  7.7800000*0.868*0.868},
 {"mH50_mSusy800",  7.7800000*0.858*0.858},
+{"mH60_mSusy800",  7.7800000*0.850*0.850},
 {"mH70_mSusy800",  7.7800000*0.840*0.840},
+{"mH80_mSusy800",  7.7800000*0.829*0.829},
 {"mH90_mSusy800",  7.7800000*0.816*0.816},
+{"mH100_mSusy800",  7.7800000*0.795*0.795},
 {"mH110_mSusy800", 7.7800000*0.749*0.749},
 {"mH125_mSusy800", 7.7800000*0.581*0.581},
 
@@ -184,8 +211,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy1200", 0.5794000*0.867*0.867},
 {"mH40_mSusy1200", 0.5794000*0.865*0.865},
 {"mH50_mSusy1200", 0.5794000*0.858*0.858},
+{"mH60_mSusy1200", 0.5794000*0.850*0.850},
 {"mH70_mSusy1200", 0.5794000*0.840*0.840},
+{"mH80_mSusy1200", 0.5794000*0.829*0.829},
 {"mH90_mSusy1200", 0.5794000*0.816*0.816},
+{"mH100_mSusy1200", 0.5794000*0.795*0.795},
 {"mH110_mSusy1200",0.5794000*0.749*0.749},
 {"mH125_mSusy1200",0.5794000*0.581*0.581},
 
@@ -193,8 +223,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy1600", 0.0688200*0.867*0.867},
 {"mH40_mSusy1600", 0.0688200*0.865*0.865},
 {"mH50_mSusy1600", 0.0688200*0.858*0.858},
+{"mH60_mSusy1600", 0.0688200*0.850*0.850},
 {"mH70_mSusy1600", 0.0688200*0.840*0.840},
+{"mH80_mSusy1600", 0.0688200*0.829*0.829},
 {"mH90_mSusy1600", 0.0688200*0.816*0.816},
+{"mH100_mSusy1600", 0.0688200*0.795*0.795},
 {"mH110_mSusy1600",0.0688200*0.749*0.749},
 {"mH125_mSusy1600",0.0688200*0.581*0.581},
 
@@ -202,8 +235,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy2000", 0.0101300*0.867*0.867},
 {"mH40_mSusy2000", 0.0101300*0.865*0.865},
 {"mH50_mSusy2000", 0.0101300*0.858*0.858},
+{"mH60_mSusy2000", 0.0101300*0.850*0.850},
 {"mH70_mSusy2000", 0.0101300*0.840*0.840},
+{"mH80_mSusy2000", 0.0101300*0.829*0.829},
 {"mH90_mSusy2000", 0.0101300*0.816*0.816},
+{"mH100_mSusy2000", 0.0101300*0.795*0.795},
 {"mH110_mSusy2000",0.0101300*0.749*0.749},
 {"mH125_mSusy2000",0.0101300*0.581*0.581},
 
@@ -211,8 +247,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy2200", 0.0040540*0.867*0.867},
 {"mH40_mSusy2200", 0.0040540*0.865*0.865},
 {"mH50_mSusy2200", 0.0040540*0.858*0.858},
+{"mH60_mSusy2200", 0.0040540*0.850*0.850},
 {"mH70_mSusy2200", 0.0040540*0.840*0.840},
+{"mH80_mSusy2200", 0.0040540*0.829*0.829},
 {"mH90_mSusy2200", 0.0040540*0.816*0.816},
+{"mH100_mSusy2200", 0.0040540*0.795*0.795},
 {"mH110_mSusy2200",0.0040540*0.749*0.749},
 {"mH125_mSusy2200",0.0040540*0.581*0.581},
 
@@ -220,8 +259,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy2400", 0.0016373*0.867*0.867},
 {"mH40_mSusy2400", 0.0016373*0.865*0.865},
 {"mH50_mSusy2400", 0.0016373*0.858*0.858},
+{"mH60_mSusy2400", 0.0016373*0.850*0.850},
 {"mH70_mSusy2400", 0.0016373*0.840*0.840},
+{"mH80_mSusy2400", 0.0016373*0.829*0.829},
 {"mH90_mSusy2400", 0.0016373*0.816*0.816},
+{"mH100_mSusy2400", 0.0016373*0.795*0.795},
 {"mH110_mSusy2400",0.0016373*0.749*0.749},
 {"mH125_mSusy2400",0.0016373*0.581*0.581},
 
@@ -229,8 +271,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy2600", 0.0006666*0.867*0.867},
 {"mH40_mSusy2600", 0.0006666*0.865*0.865},
 {"mH50_mSusy2600", 0.0006666*0.858*0.858},
+{"mH60_mSusy2600", 0.0006666*0.850*0.850},
 {"mH70_mSusy2600", 0.0006666*0.840*0.840},
+{"mH80_mSusy2600", 0.0006666*0.829*0.829},
 {"mH90_mSusy2600", 0.0006666*0.816*0.816},
+{"mH100_mSusy2600", 0.0006666*0.795*0.795},
 {"mH110_mSusy2600",0.0006666*0.749*0.749},
 {"mH125_mSusy2600",0.0006666*0.581*0.581},
 
@@ -238,8 +283,11 @@ std::map<std::string, double> xsecBRNNLL{
 {"mH35_mSusy2800", 0.0002707*0.867*0.867},
 {"mH40_mSusy2800", 0.0002707*0.865*0.865},
 {"mH50_mSusy2800", 0.0002707*0.858*0.858},
+{"mH60_mSusy2800", 0.0002707*0.850*0.850},
 {"mH70_mSusy2800", 0.0002707*0.840*0.840},
+{"mH80_mSusy2800", 0.0002707*0.829*0.829},
 {"mH90_mSusy2800", 0.0002707*0.816*0.816},
+{"mH100_mSusy2800", 0.0002707*0.795*0.795},
 {"mH110_mSusy2800",0.0002707*0.749*0.749},
 {"mH125_mSusy2800",0.0002707*0.581*0.581},
 
@@ -407,18 +455,24 @@ int main(){
     // const std::string inputDir = "/opt/ppd/scratch/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20200527/combinedDataCards_ht_XSjmsryear_newZJ_2017as2018_0.98_allSig_ecalfilter_QCDlb0.1tunedubtuned5_bkg10pc_unccorrelated_maxunc2_jmrsymuncor_symall1.00.01";
     
     // const std::string inputDir = "/mercury/data2/linacre/NMSSM/Analysis_boostedNmssmHiggs/combinedDataCards_20200527/combinedDataCards_ht_XSjmsryear_newZJ_2017as2018_0.98_allSig_ecalfilter_QCDlb0.1tunedubtuned5_bkg10pc_unccorrelated_maxunc2_jmrsymuncor_symall1.00.01";
-    const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210225/combinedDataCards_ht_XSjmsryear_newZJ_2017as2018sqfix_0.98_allSig_ecalfilter_QCDlb0.0tunedubtuned5_bkg10pc_unccorrelated_maxunc2_jmrsymuncor_symall1.00.01/Asymptotic_copy";
+    // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210225/combinedDataCards_ht_XSjmsryear_newZJ_2017as2018sqfix_0.98_allSig_ecalfilter_QCDlb0.0tunedubtuned5_bkg10pc_unccorrelated_maxunc2_jmrsymuncor_symall1.00.01/Asymptotic_copy";
+    // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210622/combinedDataCards_10bins_lnNforQCD_interpolated9_batch";
+    // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210622/combinedDataCards_10bins_lnNforQCD_interpolated9d_jmsrdbt1718corr_batch";
+    // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210622/combinedDataCards_10bins_lnNforQCD_interpolated9d_dbtcorr_batch";
+    // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210622/combinedDataCards_10bins_lnNforQCD_interpolated9d_jmsrcor_batch";
+    // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210622/combinedDataCards_10bins_lnNforQCD_interpolated9d_jmsrdbtcorr_batch";
+    const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210622/combinedDataCards_10bins_lnNforQCD_interpolated9dcff_batch";
 
     // const std::string inputDir = "/opt/ppd/scratch-2021/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20210204/combinedDataCards_ht_XSjmsryear_newZJ_2018_2017as2018sqtest2g_0.98_allSig_ecalfilter_QCDlb0.1tunedubtuned5_bkg10pc_unccorrelated_maxunc2_jmrsymuncor_symall1.00.01";
     // const std::string inputDir = "/opt/ppd/scratch/xxt18833/Analysis_boostedNmssmHiggs/combinedDataCards_20200120/combinedDataCards_final_2018_2017as2018_0.98_allSigs";
     // const std::string inputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/combinedDataCards_2019_01_01/withGluino/allSys/";
 
     // TWO: plot output directory
-    std::string outputDir = "brazilplots_unblinded_NNLLth";
+    std::string outputDir = "brazilplots_unblinded_NNLLth_interpolated9d_newhdecay_defaultcor";
     // const std::string outputDir = "/opt/ppd/scratch/xap79297/Analysis_boostedNmssmHiggs/plots_2019_01_01/brazilplots/mSusy2400/";
 
     // THREE: higgs and SUSY masses (one of which should have a single entry)
-    const std::vector<int> higgsMasses = {30, 35, 40, 50, 70, 90, 110, 125};
+    const std::vector<int> higgsMasses = {30, 35, 40, 50, 60, 70, 80, 90, 100, 110, 125};
     //const std::vector<int> susyMasses = {2600};
     // const std::vector<int> higgsMasses = {90};
     // const std::vector<int> susyMasses = {1200, 1600, 2000, 2200, 2400, 2600, 2800};
@@ -537,8 +591,8 @@ int main(){
 
         yTh_vec.push_back(xBRNNLL);
 
-        yThDown1Sig_vec.push_back( xBRNNLL * xsecUnc[varyHiggsMass ? fixedMass : massPoint] );
-        yThUp1Sig_vec.push_back( xBRNNLL * xsecUnc[varyHiggsMass ? fixedMass : massPoint] );
+        yThDown1Sig_vec.push_back( xBRNNLL - xBRNNLL * xsecUnc[varyHiggsMass ? fixedMass : massPoint] );
+        yThUp1Sig_vec.push_back( xBRNNLL + xBRNNLL * xsecUnc[varyHiggsMass ? fixedMass : massPoint] );
     }     
 
     TGraphAsymmErrors * g_obs = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(yObs_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]));
@@ -551,15 +605,18 @@ int main(){
     TGraphAsymmErrors * g_expErr2Sig = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(y_vec[0]), &(null_vec[0]), &(null_vec[0]), &(yErrDown2Sig_vec[0]), &(yErrUp2Sig_vec[0]));
 
     TGraphAsymmErrors * g_th = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(yTh_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]));
-    TGraphAsymmErrors * g_thErr1Sig = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(yTh_vec[0]), &(null_vec[0]), &(null_vec[0]), &(yThDown1Sig_vec[0]), &(yThUp1Sig_vec[0]));
+    TGraphAsymmErrors * g_thDown1Sig = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(yThDown1Sig_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]));
+    TGraphAsymmErrors * g_thUp1Sig = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(yThUp1Sig_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]), &(null_vec[0]));
+
+    // TGraphAsymmErrors * g_thErr1Sig = new TGraphAsymmErrors(nEntries, &(x_vec[0]), &(yTh_vec[0]), &(null_vec[0]), &(null_vec[0]), &(yThDown1Sig_vec[0]), &(yThUp1Sig_vec[0]));
 
     if(fixedYRange) {
-        maxLimitValue = 0.06;
+        maxLimitValue = 0.09;
         minLimitValue = 0.00006;
     }
 
     // the vector order goes: observed, expected, 1sigma, 2sigma, th, 1sigma
-    Plotter brazilPlot = Plotter({g_obs, g_exp, g_expErr1Sig, g_expErr2Sig, g_th, g_thErr1Sig}, plotObserved);
+    Plotter brazilPlot = Plotter({g_obs, g_exp, g_expErr1Sig, g_expErr2Sig, g_th, g_thDown1Sig, g_thUp1Sig}, plotObserved);
     // brazilPlot.AddLegend(0.20, 0.45, 0.63, 0.86);
     brazilPlot.AddLegend(0.60, 0.85, 0.63, 0.86);
     brazilPlot.AddLatex(luminosity, "#it{Preliminary}");
